23408e20613a940fc4fefd1bc6759b2533901893
yolandafloresc14 <yolanda141002@gmail.com>
2024-11-29 13:00:09 -0500

    reporte y home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.10.0")
 }
